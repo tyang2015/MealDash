b7c5fb1e98b5ed3f00b9ae0c6f58898710690552
tiffany yang <tiffanyang2015@gmail.com>
2022-09-29 10:46:52 -0700

    pushing again

--- react-app/src/components/GetRestaurant/index.js
+++ react-app/src/components/GetRestaurant/index.js
@@ -2,6 +2,7 @@ import { useParams, useHistory, NavLink } from "react-router-dom"
 import React,{useState, useEffect} from "react"
 import { useDispatch, useSelector } from "react-redux";
 import { getAllRestaurants } from "../../store/restaurant";
+import "./GetRestaurant.css"
 
 const GetRestaurant = () => {
   let { id } = useParams();
@@ -22,7 +23,9 @@ const GetRestaurant = () => {
           <div className="restaurant-page-pic-container">
             <img className="restaurant-page-pic" src={restaurant.restaurantPicUrl} onError={e => { e.currentTarget.src = "https://i.pinimg.com/originals/90/85/b0/9085b0692d8ffe530e71a601ec887cf2.jpg"; }}/>
           </div>
-          <img className="restaurant-page-logo-pic" src={restaurant.logo} onError={e => { e.currentTarget.src = "https://cdn5.vectorstock.com/i/1000x1000/65/29/vintage-badge-retro-blank-labels-logo-vector-23946529.jpg"; }} />
+          <div className="restaurant-page-logo-pic-container">
+            <img className="restaurant-page-logo-pic" src={restaurant.logo} onError={e => { e.currentTarget.src = "https://cdn5.vectorstock.com/i/1000x1000/65/29/vintage-badge-retro-blank-labels-logo-vector-23946529.jpg"; }} />
+          </div>
           <div className="restaurant-page-name">
             {restaurant.name}
           </div>
